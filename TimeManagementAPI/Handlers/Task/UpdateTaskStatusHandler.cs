118b9683bd9f995767afc2de72dcb4605095d3c3
Jaime Reis <jaime_reis18@outlook.pt>
2022-01-17 20:47:58 -0100

    Creation of handler for updating a task status

--- TimeManagementAPI/Handlers/Task/UpdateTaskStatusHandler.cs
+++ TimeManagementAPI/Handlers/Task/UpdateTaskStatusHandler.cs
@@ -2,15 +2,15 @@
 using System.Threading;
 using System.Threading.Tasks;
 using TimeManagementAPI.Commands.Task;
-using TimeManagementAPI.Repositories;
+using TimeManagementAPI.Repositories.Interfaces;
 
 namespace TimeManagementAPI.Handlers.Task
 {
     public class UpdateTaskStatusHandler : IRequestHandler<UpdateTaskStatusCommand>
     {
-        private readonly TaskRepository _taskRepository;
+        private readonly ITaskRepository _taskRepository;
 
-        public UpdateTaskStatusHandler(TaskRepository taskRepository)
+        public UpdateTaskStatusHandler(ITaskRepository taskRepository)
         {
             _taskRepository = taskRepository;
         }
