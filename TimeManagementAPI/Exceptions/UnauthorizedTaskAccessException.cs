38794a41fefdd645da4cf11a55a2a1d66383680e
Jaime Reis <jaime_reis18@outlook.pt>
2022-01-23 17:57:37 -0100

    Creation of unauthorized task access exception

---
+++ TimeManagementAPI/Exceptions/UnauthorizedTaskAccessException.cs
@@ -0,0 +1,13 @@
+﻿namespace TimeManagementAPI.Exceptions
+{
+    public class UnauthorizedTaskAccessException : BaseException
+    {
+        public UnauthorizedTaskAccessException() : base("Unauthorized task access", 401)
+        {
+        }
+
+        public UnauthorizedTaskAccessException(string message) : base(message, 404)
+        {
+        }
+    }
+}
