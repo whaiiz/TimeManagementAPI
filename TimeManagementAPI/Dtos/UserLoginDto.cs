55bdc53f8802d617f8b0b210c6e3207c2f794063
Jaime Reis <jaime_reis18@outlook.pt>
2022-01-24 22:42:51 -0100

    Creation of models for register and login

---
+++ TimeManagementAPI/Dtos/UserLoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeManagementAPI.Dtos
+{
+    public class UserLoginDto
+    {
+        [Required(ErrorMessage = "The username is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "The password is required")]
+        public string Password { get; set; }
+    }
+}
