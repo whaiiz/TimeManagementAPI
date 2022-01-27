eab3bfa97473761c6f7cbd5b45d14dbd1dc57070
Jaime Reis <jaime_reis18@outlook.pt>
2022-01-27 19:44:54 -0100

    Creation of ConfirmEmailCommand and it's ghandler

---
+++ TimeManagementAPI/Commands/Authentication/ConfirmEmailCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using TimeManagementAPI.Utils;
+
+namespace TimeManagementAPI.Commands.Authentication
+{
+    public record ConfirmEmailCommand(string Username, string Token) : IRequest<ResponseModel>;
+}
