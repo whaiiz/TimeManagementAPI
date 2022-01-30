25ce6e656040ca6e5b62f11f6e7116f85b066196
Jaime Reis <jaime_reis18@outlook.pt>
2022-01-30 12:17:20 -0100

    Created a messages class that stores all the messages of the application

---
+++ TimeManagementAPI/Utils/Messages.cs
@@ -0,0 +1,25 @@
+﻿namespace TimeManagementAPI.Utils
+{
+    public static class Messages
+    {
+        /* Generic messages */
+        public const string UnexpectedError = "Something went wrong!";
+        
+        /* Email confirmation messages */
+        public const string EmailConfirmed = "Your email was confirmed with success!";
+        public const string InvalidEmailConfirmationToken = "The token sent is invalid or it has already expired!";
+        
+        /* Login messages */
+        public const string UserDoesntExist = "The user doesn't exist!";
+        public const string WrongPassword = "Wrong password!";
+        public const string ConfirmYourEmail = "Confirm your email first!";
+        public const string ErrorSendingEmailConfirmationOnLogin = "Error sending email confirmation! Please contact support.";
+
+        /* Register messages */
+        public const string UsernameAlreadyExists = "Error sending email confirmation!";
+        public const string EmailAlreadyExists = "Error sending email confirmation!";
+        public const string ErrorSendingEmailConfirmationOnRegister = "User registered with success! but there was an " +
+            "error sending email confirmation! Please try to log in.";
+        public const string UserRegisteredWithSuccess = "User registered with success! Please go to your email to activate your account.";
+    }
+}
