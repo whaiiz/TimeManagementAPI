66005d65721f80214f5a96e4fe32fad0ca52f73a
Jaime Reis <42680567+whaiiz@users.noreply.github.com>
2022-02-05 20:39:03 -0100

    Fixed messages

--- TimeManagementAPI/Utils/Messages.cs
+++ TimeManagementAPI/Utils/Messages.cs
@@ -7,7 +7,7 @@
         
         /* Email confirmation messages */
         public const string EmailConfirmed = "Your email was confirmed with success!";
-        public const string EmailConfirmation = "Email Confirmation"
+        public const string EmailConfirmation = "Email Confirmation";
         public const string InvalidEmailConfirmationToken = "The token sent is invalid or it has already expired!";
         
         /* Login messages */
@@ -17,8 +17,8 @@
         public const string ErrorSendingEmailConfirmationOnLogin = "Error sending email confirmation! Please contact support.";
 
         /* Register messages */
-        public const string UsernameAlreadyExists = "Error sending email confirmation!";
-        public const string EmailAlreadyExists = "Error sending email confirmation!";
+        public const string UsernameAlreadyExists = "Username already exists!";
+        public const string EmailAlreadyExists = "Email already exists!";
         public const string ErrorSendingEmailConfirmationOnRegister = "User registered with success! but there was an " +
             "error sending email confirmation! Please try to log in.";
         public const string UserRegisteredWithSuccess = "User registered with success! Please go to your email to activate your account.";
