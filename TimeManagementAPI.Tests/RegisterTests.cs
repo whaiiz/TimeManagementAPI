90320379ccb0fd3fdc43f4b6165d8d72400c183a
Jaime Reis <42680567+whaiiz@users.noreply.github.com>
2022-02-13 11:34:09 -0100

    Created RegisterTests.cs

---
+++ TimeManagementAPI.Tests/RegisterTests.cs
@@ -0,0 +1,48 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TimeManagementAPI.Controllers;
+using TimeManagementAPI.Dtos;
+using Xunit;
+
+namespace TimeManagementAPI.Tests
+{
+    public class RegisterTests
+    {
+        [Fact]
+        public async Task Register_UsernameShouldBeRequired([FromServices] IMediator mediator)
+        {
+
+
+
+        }
+        public void Register_EmailShouldBeRequired()
+        {
+
+        }
+
+        public void Register_EmailShouldHaveAValidFormat()
+        {
+
+        }
+
+        public void Register_PasswordShouldBeRequired()
+        {
+
+        }
+
+        public void Register_UsernameShouldBeUnique()
+        {
+
+        }
+        public void Register_EmailShouldBeUnique()
+        {
+
+        }
+
+        public void Register_ShouldWork()
+        {
+
+        }
+
+    }
+}
