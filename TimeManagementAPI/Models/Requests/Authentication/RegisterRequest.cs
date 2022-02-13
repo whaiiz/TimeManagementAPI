337e64410f657befa8195f16b166528c0db28a2e
Jaime Reis <42680567+whaiiz@users.noreply.github.com>
2022-02-13 14:20:49 -0100

    Created models for login and register requests

---
+++ TimeManagementAPI/Models/Requests/Authentication/RegisterRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeManagementAPI.Models.Requests.Authentication
+{
+    public class RegisterRequest
+    {
+        [Required(ErrorMessage = "The username is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "The email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The password is required")]
+        public string Password { get; set; }
+    }
+}
