1c7e0d2e5bca5a3afa13a5fbd4a81aa32f2b7b09
Jaime Reis <42680567+whaiiz@users.noreply.github.com>
2022-02-15 21:31:16 -0100

    Creation of Register_UsernameShouldBeUnique test

---
+++ TimeManagementAPI.Tests/Controllers/RegisterTests.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using TimeManagementAPI.Commands.Authentication;
+using TimeManagementAPI.Controllers;
+using TimeManagementAPI.Models.Requests.Authentication;
+using TimeManagementAPI.Utils;
+using Xunit;
+
+namespace TimeManagementAPI.Tests
+{
+    public class RegisterTests
+    {
+        [Fact]
+        public async Task Register_UsernameShouldBeUnique()
+        {
+            // Arrange
+            var mediator = new Mock<IMediator>();
+            var controller = new AuthenticationController(mediator.Object);
+            var user = new RegisterRequest()
+            {
+                Password = "Teste",
+                Email = "Teste@gmail.com",
+            };
+
+            mediator.Setup(m => m.Send(It.IsAny<RegisterCommand>(), default(CancellationToken)))
+                .ReturnsAsync(new ResponseModel(400, "Username already exists!"));
+
+            // Act
+            var result = await controller.Register(user);
+
+            // Assert
+            var response = Assert.IsType<ObjectResult>(result);
+            Assert.Equal("Username already exists!", response.Value);
+            Assert.Equal(400, response.StatusCode);
+        }
+    }
+}
