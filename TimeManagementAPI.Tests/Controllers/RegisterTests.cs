b8f320dd14bd447323e814ebbd13aa956578ad3f
Jaime Reis <42680567+whaiiz@users.noreply.github.com>
2022-02-15 21:57:09 -0100

    Creation of Register_FailSendingEmailConfirmation test

--- TimeManagementAPI.Tests/Controllers/RegisterTests.cs
+++ TimeManagementAPI.Tests/Controllers/RegisterTests.cs
@@ -12,7 +12,7 @@ namespace TimeManagementAPI.Tests
     public class RegisterTests
     {
         [Fact]
-        public async Task Register_UsernameShouldBeUnique()
+        public async Task Register_NotUniqueUsername()
         {
             // Arrange
             var mediator = new Mock<IMediator>();
@@ -21,6 +21,7 @@ namespace TimeManagementAPI.Tests
             {
                 Password = "Teste",
                 Email = "Teste@gmail.com",
+                Username = "Teste@gmail.com",
             };
 
             mediator.Setup(m => m.Send(It.IsAny<RegisterCommand>(), default))
@@ -36,7 +37,7 @@ namespace TimeManagementAPI.Tests
         }
 
         [Fact]
-        public async Task Register_EmailShouldBeUnique()
+        public async Task Register_NotUniqueEmail()
         {
             // Arrange
             var mediator = new Mock<IMediator>();
@@ -45,6 +46,7 @@ namespace TimeManagementAPI.Tests
             {
                 Password = "Teste",
                 Email = "Teste@gmail.com",
+                Username = "Teste",
             };
 
             mediator.Setup(m => m.Send(It.IsAny<RegisterCommand>(), default))
@@ -58,5 +60,33 @@ namespace TimeManagementAPI.Tests
             Assert.Equal("Email already exists!", response.Value);
             Assert.Equal(400, response.StatusCode);
         }
+        [Fact]
+        public async Task Register_FailSendingEmailConfirmation()
+        {
+            // Arrange
+            var mediator = new Mock<IMediator>();
+            var controller = new AuthenticationController(mediator.Object);
+            var user = new RegisterRequest()
+            {
+                Password = "Teste",
+                Email = "Teste@gmail.com",
+                Username = "Teste",
+            };
+
+            mediator.Setup(m => m.Send(It.IsAny<RegisterCommand>(), default))
+                .ReturnsAsync(new ResponseModel(400, 
+                "User registered with success! but there was an " +
+                "error sending email confirmation! Please try to log in."!));
+
+            // Act
+            var result = await controller.Register(user);
+
+            // Assert
+            var response = Assert.IsType<ObjectResult>(result);
+            Assert.Equal("User registered with success! but there was an " +
+                "error sending email confirmation! Please try to log in.", response.Value);
+            Assert.Equal(400, response.StatusCode);
+        }
     }
+
 }
