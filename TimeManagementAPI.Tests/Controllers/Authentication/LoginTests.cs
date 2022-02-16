600c584d361f3d30ac987a631a560fec66aa2424
Jaime Reis <42680567+whaiiz@users.noreply.github.com>
2022-02-16 20:46:20 -0100

    Creation of all login tests for controller

--- TimeManagementAPI.Tests/Controllers/Authentication/LoginTests.cs
+++ TimeManagementAPI.Tests/Controllers/Authentication/LoginTests.cs
@@ -41,5 +41,77 @@ namespace TimeManagementAPI.Tests.Controllers.Authentication
             Assert.Equal("The user doesn't exist!", response.Value);
             Assert.Equal(400, response.StatusCode);
         }
+
+        [Fact]
+        public async Task Login_PasswordNotCorrect()
+        {
+            // Arrange
+            var (mediator, controller, user) = GetBaseMocks();
+
+            mediator.Setup(m => m.Send(It.IsAny<LoginCommand>(), default))
+                .ReturnsAsync(new ResponseModel(400, "Wrong password!"));
+
+            // Act
+            var result = await controller.Login(user);
+
+            // Assert
+            var response = Assert.IsType<ObjectResult>(result);
+            Assert.Equal("Wrong password!", response.Value);
+            Assert.Equal(400, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Login_EmailNotConfirmed()
+        {
+            // Arrange
+            var (mediator, controller, user) = GetBaseMocks();
+
+            mediator.Setup(m => m.Send(It.IsAny<LoginCommand>(), default))
+                .ReturnsAsync(new ResponseModel(400, "Confirm your email first!"));
+
+            // Act
+            var result = await controller.Login(user);
+
+            // Assert
+            var response = Assert.IsType<ObjectResult>(result);
+            Assert.Equal("Confirm your email first!", response.Value);
+            Assert.Equal(400, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Login_FailedSendingConfirmationEmail()
+        {
+            // Arrange
+            var (mediator, controller, user) = GetBaseMocks();
+
+            mediator.Setup(m => m.Send(It.IsAny<LoginCommand>(), default))
+                .ReturnsAsync(new ResponseModel(400, "Error sending email confirmation! Please contact support."));
+
+            // Act
+            var result = await controller.Login(user);
+
+            // Assert
+            var response = Assert.IsType<ObjectResult>(result);
+            Assert.Equal("Error sending email confirmation! Please contact support.", response.Value);
+            Assert.Equal(400, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Login_Success()
+        {
+            // Arrange
+            var (mediator, controller, user) = GetBaseMocks();
+
+            mediator.Setup(m => m.Send(It.IsAny<LoginCommand>(), default))
+                .ReturnsAsync(new ResponseModel(200, "token"));
+
+            // Act
+            var result = await controller.Login(user);
+
+            // Assert
+            var response = Assert.IsType<ObjectResult>(result);
+            Assert.Equal("token", response.Value);
+            Assert.Equal(200, response.StatusCode);
+        }
     }
 }
