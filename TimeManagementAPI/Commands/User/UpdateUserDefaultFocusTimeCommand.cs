448a9d57a6794263c075741197cc373ce6edd94c
Jaime Reis <42680567+whaiiz@users.noreply.github.com>
2022-03-03 21:42:34 -0100

    Creation of UpdateUserDefaultFocusTime Handler and Command

---
+++ TimeManagementAPI/Commands/User/UpdateUserDefaultFocusTimeCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace TimeManagementAPI.Commands.User
+{
+    public record UpdateUserDefaultFocusTimeCommand(string Username, int FocusTime) : IRequest;
+}
---
+++ TimeManagementAPI/Handlers/User/UpdateUserDefaultFocusTimeHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using TimeManagementAPI.Commands.User;
+using TimeManagementAPI.Exceptions;
+using TimeManagementAPI.Repositories.Interfaces;
+
+namespace TimeManagementAPI.Handlers.User
+{
+    public class UpdateUserDefaultFocusTimeHandler : IRequestHandler<UpdateUserDefaultFocusTimeCommand>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UpdateUserDefaultFocusTimeHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<Unit> Handle(UpdateUserDefaultFocusTimeCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByUsername(request.Username);
+
+            if (user == null) throw new UserNotFoundException();
+
+            user.DefaultFocusTime = request.FocusTime;
+            await _userRepository.Update(user);
+
+            return Unit.Value;
+        }
+    }
+}
