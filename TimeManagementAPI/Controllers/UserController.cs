3dafe46e6bdb99a80535fbf93b4bfcc19b3de2b8
Jaime Reis <42680567+whaiiz@users.noreply.github.com>
2022-03-03 21:43:02 -0100

    Creation of user controller

---
+++ TimeManagementAPI/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using MediatR;
+using TimeManagementAPI.Filters;
+using Microsoft.AspNetCore.Authorization;
+using TimeManagementAPI.Commands.User;
+
+namespace TimeManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    [CustomExceptionFilter]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateUserDefaultFocusTime(int focusTime)
+        {
+            await _mediator.Send(new UpdateUserDefaultFocusTimeCommand(User.Identity.Name, focusTime));
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateUserDefaultBreakTime(int breakTime)
+        {
+            await _mediator.Send(new UpdateUserDefaultFocusTimeCommand(User.Identity.Name, breakTime));
+            return Ok();
+        }
+    }
+}
