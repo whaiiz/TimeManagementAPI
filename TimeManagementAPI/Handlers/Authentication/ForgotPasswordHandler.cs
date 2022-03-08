a9176ee5822038d51b5bfa7b633031d83d53d021
Jaime Reis <42680567+whaiiz@users.noreply.github.com>
2022-03-08 20:26:56 -0100

    Changed expiration time tokens of email confrimation and password reset

--- TimeManagementAPI/Handlers/Authentication/ForgotPasswordHandler.cs
+++ TimeManagementAPI/Handlers/Authentication/ForgotPasswordHandler.cs
@@ -37,7 +37,7 @@ namespace TimeManagementAPI.Handlers.Authentication
             var claims = new List<Claim> { new Claim(ClaimTypes.Name, user.Username) };
             var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("JwtTokenKey").Value));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
-            var token = new JwtSecurityToken(claims: claims, expires: DateTime.Now.AddHours(2), signingCredentials: creds);
+            var token = new JwtSecurityToken(claims: claims, expires: DateTime.Now.AddMinutes(15), signingCredentials: creds);
             var jwt = new JwtSecurityTokenHandler().WriteToken(token);
 
             return jwt;
