11b21763a8e5739a92f89212fd921edf914dca67
Jaime Reis <42680567+whaiiz@users.noreply.github.com>
2022-03-12 21:30:43 -0100

    Creation of RefreshTokenResponse and Handler

--- TimeManagementAPI/Handlers/Authentication/RefreshTokenHandler.cs
+++ TimeManagementAPI/Handlers/Authentication/RefreshTokenHandler.cs
@@ -8,19 +8,28 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using TimeManagementAPI.Commands.Authentication;
+using TimeManagementAPI.Exceptions;
+using TimeManagementAPI.Models.Responses;
+using TimeManagementAPI.Repositories.Interfaces;
+using TimeManagementAPI.Utils;
 
 namespace TimeManagementAPI.Handlers.Authentication
 {
-    public class RefreshTokenHandler : IRequestHandler<RefreshTokenCommand, bool>
+    public class RefreshTokenHandler : IRequestHandler<RefreshTokenCommand, RefreshTokenResponse>
     {
         private readonly IConfiguration _configuration;
+        private readonly IUserRepository _userRepository;
+        private readonly IMediator _mediator;
 
-        public RefreshTokenHandler(IConfiguration configuration)
+        public RefreshTokenHandler(IConfiguration configuration, IUserRepository userRepository,
+            IMediator mediator)
         {
             _configuration = configuration;
+            _userRepository = userRepository;
+            _mediator = mediator;
         }
 
-        private ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+        private ClaimsPrincipal GetClaimsFromExpiredAccessToken(string accessToken)
         {
             var tokenValidationParameters = new TokenValidationParameters
             {
@@ -31,18 +40,36 @@ namespace TimeManagementAPI.Handlers.Authentication
                 ValidateLifetime = false
             };
             var tokenHandler = new JwtSecurityTokenHandler();
-            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken securityToken);
+            var principal = tokenHandler.ValidateToken(accessToken, tokenValidationParameters, out SecurityToken securityToken);
             
             if (securityToken is not JwtSecurityToken jwtSecurityToken || !jwtSecurityToken.Header.Alg.Equals(
-                SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
-                throw new SecurityTokenException("Invalid token");
+                SecurityAlgorithms.HmacSha512Signature, StringComparison.InvariantCultureIgnoreCase))
+                throw new SecurityTokenException(Messages.InvalidToken);
 
             return principal;
         }
 
-        public Task<bool> Handle(RefreshTokenCommand request, CancellationToken cancellationToken)
+        public async Task<RefreshTokenResponse> Handle(RefreshTokenCommand request, CancellationToken cancellationToken)
         {
-            throw new System.NotImplementedException();
+            var principal = GetClaimsFromExpiredAccessToken(request.AccessToken);
+            var username = principal.Identity.Name;
+            var user = await _userRepository.GetByUsername(username);
+
+            if (user == null) throw new UserNotFoundException();
+            if (user.RefreshToken != request.RefreshToken) throw new InvalidTokenException();
+            if (user.RefreshTokenExpiryTime <= DateTime.Now) return new RefreshTokenResponse()
+            {
+                RefreshTokenExpired = true,
+                Message = Messages.RefreshTokenExpired,
+                StatusCode = 400
+            };
+
+            return new RefreshTokenResponse()
+            {
+                StatusCode = 200,
+                RefreshToken = await _mediator.Send(new GenerateRefreshTokenCommand(user), cancellationToken),
+                AccessToken = await _mediator.Send(new GenerateAccessTokenCommand(user), cancellationToken),
+            };
         }
     }
 }
---
+++ TimeManagementAPI/Models/Responses/RefreshTokenResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace TimeManagementAPI.Models.Responses
+{
+    public class RefreshTokenResponse
+    {
+        public int StatusCode { get; set; }
+
+        public string Message { get; set; }
+
+        public string Token { get; set; }
+
+        public bool RefreshTokenExpired { get; set; }
+
+        public string RefreshToken { get; set; }
+        
+        public string AccessToken { get; set; }
+    }
+}
